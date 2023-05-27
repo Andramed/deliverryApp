8192c495ece15137ed10d28307aab500730d2667
Andramed <122152304+Andramed@users.noreply.github.com>
2023-05-27 08:42:36 +0000

    added functionality

--- public/script/index.js
+++ public/script/index.js
@@ -2,40 +2,53 @@
 import products from '../data/products.json' assert {type: "json"}
 import {giveRestaurantList} from './createListRestaurant.js';
 import { giveProductsList } from './createCardOdFood.js';
+import fs from 'fs'
 
+
+
+let selectedRestaurant = 'KFC';
+let oldRestaurant = 'KFC';
+let listOfBuyedFood = [];
+giveProductsList(selectedRestaurant, products);
+giveRestaurantList(products);
 
 let numberOfBuyedProducts = document.querySelector('#counerOrderedProduct');
 let containerProduct = document.querySelector('.cardContainer');
-let selectedRestaurant = ''
+
 let counterProductBuyed = 0
-
-
-
-
-
-  
-  giveProductsList(null, products);
-  giveRestaurantList(products);
- 
-  let  btn = document.querySelectorAll('.btnRestaurant');
-  let btnArray = Array.from(btn);
-  
- 
+let  btn = document.querySelectorAll('.btnRestaurant');
+let btnArray = Array.from(btn);
+containerProduct.addEventListener('click', (event) => {
+    let elementClicked = event.target.textContent
+    if (elementClicked === 'order') {
+        let parentId = Number(event.target.parentNode.parentNode.id);
+        counterProductBuyed++
+        numberOfBuyedProducts.textContent = counterProductBuyed;
+        numberOfBuyedProducts.classList.remove('hidden'); 
+        let buyedItem = {
+          restaurant: selectedRestaurant,
+          idFood: parentId
+        }
+        listOfBuyedFood.push(buyedItem)
+        console.log(listOfBuyedFood);
+    }
+})
  
 btnArray.forEach(button => {
   button.addEventListener('click', (event) => {
-  selectedRestaurant = event.target.textContent;
+        oldRestaurant = selectedRestaurant
+        selectedRestaurant = event.target.textContent;
         giveProductsList(selectedRestaurant, products);
+        if(selectedRestaurant != oldRestaurant) {
+          counterProductBuyed = 0
+          numberOfBuyedProducts.textContent = counterProductBuyed;
+          numberOfBuyedProducts.classList.add('hidden');
+          listOfBuyedFood = []
+        }
   });
 });
-  
-containerProduct.addEventListener('click', (event) => {
-  let elementClicked = event.target.textContent
-  if (elementClicked === 'order') {
-    let parentId = event.target.parentNode.parentNode.id;
-    if(parentId.to)
-  }
-})
+
+console.log(oldRestaurant, selectedRestaurant);
   
 
 
